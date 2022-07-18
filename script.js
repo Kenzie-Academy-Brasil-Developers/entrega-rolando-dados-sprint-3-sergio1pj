8105acec05d277f51cff78951aefb434d12b5c82
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-18 08:58:38 -0300

    Add as funções principais

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+const count = new Array(12).fill(0);
+function rollOneDice(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+console.log(rollOneDice(1,6))
+function rollTwoDice (min, max) {
+  return [rollOneDice(min, max), rollOneDice(min, max)]
+}
+console.log(rollTwoDice(1,6))
+function rollNTwoDice(n) {
+  let rollOfDice
+  for(let i = 0; i < n; i++) {
+    rollOfDice = rollTwoDice(1, 6)[0] + rollTwoDice(1, 6)[0]
+    count[rollOfDice - 1] = count[rollOfDice - 1] + 1;
+  }
+  return count;
+}
+console.log(rollNTwoDice(1000))
